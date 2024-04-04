e61ea58c3edafe389948d026fbbb0a97b9a3c450
FrederikFanshawe <f_sommerbrondum@fanshaweonline.ca>
2024-04-03 21:15:40 -0400

    Chicken

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import Chicken from '.Chicken.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Chicken} className="App-logo" alt="logo" />
         <p>
           Frederik Sommer Brondum.
         </p>
